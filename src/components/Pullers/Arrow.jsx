5e4d62588889f52a3ad1ea4ef6c2a0e5805a69b6
grebennikovdevstark <a-greb@yandex.ru>
2024-06-07 15:01:54 +0300

    pull arrow refactor

--- src/components/Pullers/Arrow.jsx
+++ src/components/Pullers/Arrow.jsx
@@ -1,44 +1,60 @@
 import React, { useState } from "react";
 import * as THREE from "three";
+import { Line } from "@react-three/drei";
 
 export default function Arrow({ direction = "x" }) {
   const a = 0.1; // halfArrow
+
+  const points = [
+    [a, -a],
+    [a, a],
+    [a * 2, a],
+    [0, a + a * 2],
+    [-a * 2, a],
+    [-a, a],
+    [-a, -a],
+    [-a * 2, -a],
+    [0, -a - a * 2],
+    [a * 2, -a],
+    [a, -a],
+  ];
+
   let rotation = [0, 0, 0];
   if (direction === "x") {
     rotation = [0, 0, Math.PI / 2];
   } else if (direction === "z") {
     rotation = [Math.PI / 2, 0, 0];
   }
-  const [opacity, setOpacity] = useState(0.2);
+  const [thickness, setThickness] = useState(1);
+
   const arrow = new THREE.Shape();
-  arrow.moveTo(a, -a);
-  arrow.lineTo(a, a);
-  arrow.lineTo(a * 2, a);
-  arrow.lineTo(0, a + a * 2);
-  arrow.lineTo(-a * 2, a);
-  arrow.lineTo(-a, a);
-  arrow.lineTo(-a, -a);
-  arrow.lineTo(-a * 2, -a);
-  arrow.lineTo(0, -a - a * 2);
-  arrow.lineTo(a * 2, -a);
+
+  arrow.moveTo(points[0].at(0), points[0].at(1));
+
+  for (let i = 1; i < 11; i++) {
+    arrow.lineTo(points[i].at(0), points[i].at(1));
+  }
 
   const extrudeSettings = {
-    depth: 0.02,
+    depth: 0,
     bevelEnabled: false,
   };
 
   return (
-    <mesh
-      rotation={rotation}
-      onPointerOver={() => {
-        setOpacity(0.8);
-      }}
-      onPointerOut={() => {
-        setOpacity(0.2);
-      }}
-    >
-      <extrudeGeometry args={[arrow, extrudeSettings]} />
-      <meshStandardMaterial color={"blue"} opacity={opacity} transparent />
-    </mesh>
+    <group>
+      <mesh
+        rotation={rotation}
+        onPointerOver={() => {
+          setThickness(3);
+        }}
+        onPointerOut={() => {
+          setThickness(1);
+        }}
+      >
+        <extrudeGeometry args={[arrow, extrudeSettings]} />
+        <meshStandardMaterial color={"blue"} opacity={0.2} transparent />
+      </mesh>
+      <Line rotation={rotation} points={points} color="blue" lineWidth={thickness} />
+    </group>
   );
 }
